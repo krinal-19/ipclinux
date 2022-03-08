b3ac92a88047ff2193c1ddc8a8d74cc3e3fa8cfd
krinal-19 <98145592+krinal-19@users.noreply.github.com>
2022-03-09 00:08:44 +0530

    Add files via upload

---
+++ a5.c
@@ -0,0 +1,34 @@
+/*
+5.Write a program that creates a file with a 4K bytes free space. (Such files are called files with holes.)
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+char buf1[]="LAB ";
+char buf2[]="OS Linux";
+
+int main(){    
+	int fd;
+	if ((fd=creat("file.txt", 0666)) < 0) {
+		printf("Creation error");
+		exit(1);
+	}
+
+	if (write(fd, buf1, sizeof(buf1)) < 0){
+	    printf("Writing error");
+	    exit(2);
+	}
+
+	if (lseek(fd, 4096, SEEK_SET) < 0){
+	    printf("Positioning error");
+	    exit(3);
+	}
+
+	if (write(fd, buf2, sizeof(buf2)) < 0){
+	    printf("Writing error");
+	    exit(2);
+	}
+}
